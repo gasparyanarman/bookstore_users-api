5c5337541bb737e66c54c35e56d046b0385cb3b3
arman <gasarman747@gmail.com>
2022-02-20 22:38:30 +0400

    Added User DAO and User DTO objects, also have implemented CreateUser and GetUser methods

--- controllers/users/users_controller.go
+++ controllers/users/users_controller.go
@@ -6,6 +6,7 @@ import (
 	"github.com/gasparyanarman/bookstore_users-api/utils/errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
 )
 
 func CreateUser(c *gin.Context) {
@@ -26,5 +27,19 @@ func CreateUser(c *gin.Context) {
 }
 
 func GetUser(c *gin.Context) {
-	c.String(http.StatusNotImplemented, "Implement me!")
+	userId, userErr := strconv.ParseInt(c.Param("user_id"), 10, 64)
+
+	if userErr != nil {
+		err := errors.NewBadRequestError("Invalid user id")
+		c.JSON(err.Status, err)
+		return
+	}
+
+	user, getError := services.GetUser(userId)
+	if getError != nil {
+		c.JSON(getError.Status, getError)
+		return
+	}
+
+	c.JSON(http.StatusOK, user)
 }
--- services/users_service.go
+++ services/users_service.go
@@ -10,5 +10,22 @@ func CreateUser(user users.User) (*users.User, *errors.RestErr) {
 		return nil, err
 	}
 
+	if err := user.Save(); err != nil {
+		return nil, err
+	}
+
 	return &user, nil
+}
+
+func GetUser(userId int64) (*users.User, *errors.RestErr) {
+	if userId <= 0 {
+		return nil, errors.NewBadRequestError("Invalid user id")
+	}
+
+	result := &users.User{Id: userId}
+	if err := result.Get(); err != nil {
+		return nil, err
+	}
+
+	return result, nil
 }
